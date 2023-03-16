d132d70eb9219794fc90b5aba501fa0fc36dbdc6
tugalsan karabacak <tugalsan@gmail.com>
2023-03-16 09:43:34 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,4 @@
 module com.tugalsan.api.charset {
     requires javax.servlet.api;
-    requires com.tugalsan.api.unsafe;
-    requires com.tugalsan.api.log;
-    requires com.tugalsan.api.executable;
     exports com.tugalsan.api.charset.client;
 }
