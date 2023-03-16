188890291f28bbc814f8327af48d06849d28aa44
tugalsan karabacak <tugalsan@gmail.com>
2023-03-16 09:49:41 +0300

    .

--- src/main/java/com/tugalsan/api/charset/client/TGS_CharSetCast.java
+++ src/main/java/com/tugalsan/api/charset/client/TGS_CharSetCast.java
@@ -2,22 +2,21 @@ package com.tugalsan.api.charset.client;
 
 import java.util.Locale;
 
+//ISSUE: https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/lang/String.html#toLowerCase()
+//ISSUE: https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/lang/String.html#toLowerCase()
 public class TGS_CharSetCast {
+    final public static boolean isTurkish = Locale.getDefault().getDisplayCountry().equals("Türkiye")
+            && Locale.getDefault().getDisplayName().equals("Türkçe (Türkiye)");
 
-    //TURKISH FIXED
-    public static String toLowerCaseFixed(CharSequence source) {
+    public static String toLowerCase(CharSequence source) {
         return isTurkish
                 ? source.toString().toLowerCase(Locale.ROOT)
                 : source.toString().toLowerCase();
     }
 
-    public static String toUpperCaseFixed(CharSequence source) {
+    public static String toUpperCase(CharSequence source) {
         return isTurkish
                 ? source.toString().toUpperCase(Locale.ROOT)
                 : source.toString().toUpperCase();
     }
-
-    //TO HOLD A GLOBAL VALUE IN YOUR PROGRAMS
-    final public static boolean isTurkish = Locale.getDefault().getDisplayCountry().equals("Türkiye")
-            && Locale.getDefault().getDisplayName().equals("Türkçe (Türkiye)");
 }
