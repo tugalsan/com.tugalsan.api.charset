f16c9c6fd53b607a7426a3508df6df55361b0bb7
tugalsan karabacak <tugalsan@gmail.com>
2023-03-16 09:53:30 +0300

    .

--- src/main/java/com/tugalsan/api/charset/client/TGS_CharSetCast.java
+++ src/main/java/com/tugalsan/api/charset/client/TGS_CharSetCast.java
@@ -5,17 +5,17 @@ import java.util.Locale;
 //ISSUE: https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/lang/String.html#toLowerCase()
 //ISSUE: https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/lang/String.html#toLowerCase()
 public class TGS_CharSetCast {
-    final public static boolean isTurkish = Locale.getDefault().getDisplayCountry().equals("Türkiye")
+    final public static boolean TURKISH = Locale.getDefault().getDisplayCountry().equals("Türkiye")
             && Locale.getDefault().getDisplayName().equals("Türkçe (Türkiye)");
 
     public static String toLowerCase(CharSequence source) {
-        return isTurkish
+        return TURKISH
                 ? source.toString().toLowerCase(Locale.ROOT)
                 : source.toString().toLowerCase();
     }
 
     public static String toUpperCase(CharSequence source) {
-        return isTurkish
+        return TURKISH
                 ? source.toString().toUpperCase(Locale.ROOT)
                 : source.toString().toUpperCase();
     }
