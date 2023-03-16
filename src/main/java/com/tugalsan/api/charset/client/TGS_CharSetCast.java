97e79935b4fa5c9f212fd63769b52d3986fad4c6
tugalsan karabacak <tugalsan@gmail.com>
2023-03-16 09:41:04 +0300

    Update TGS_CharSetCast.java

--- src/main/java/com/tugalsan/api/charset/client/TGS_CharSetCast.java
+++ src/main/java/com/tugalsan/api/charset/client/TGS_CharSetCast.java
@@ -4,29 +4,20 @@ import java.util.Locale;
 
 public class TGS_CharSetCast {
 
-    //DETECT TURKISH
-    public static boolean isLocaleDefaultTurkish() {
-        return Locale.getDefault().getDisplayCountry().equals("Türkiye")
-                && Locale.getDefault().getDisplayName().equals("Türkçe (Türkiye)");
-    }
-
     //TURKISH FIXED
     public static String toLowerCaseFixed(CharSequence source) {
-        return source.toString().toLowerCase(Locale.ROOT);
+        return isTurkish
+                ? source.toString().toLowerCase(Locale.ROOT)
+                : source.toString().toLowerCase();
     }
 
     public static String toUpperCaseFixed(CharSequence source) {
-        return source.toString().toUpperCase(Locale.ROOT);
+        return isTurkish
+                ? source.toString().toUpperCase(Locale.ROOT)
+                : source.toString().toUpperCase();
     }
 
     //TO HOLD A GLOBAL VALUE IN YOUR PROGRAMS
-    public static enum ToStringStyle {
-        TURKISH,
-        OTHER
-    }
-
-    public static boolean STYLE_TURKISH() {//DOUBLE TO STRING STYLE
-        return CAST_2_STR_STYLE == ToStringStyle.TURKISH;
-    }
-    public static ToStringStyle CAST_2_STR_STYLE = ToStringStyle.TURKISH;
+    final public static boolean isTurkish = Locale.getDefault().getDisplayCountry().equals("Türkiye")
+            && Locale.getDefault().getDisplayName().equals("Türkçe (Türkiye)");
 }
