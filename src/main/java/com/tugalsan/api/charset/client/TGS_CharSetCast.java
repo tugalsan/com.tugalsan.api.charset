2257cd4ac12c7801a530acca290ee900865f3781
tugalsan karabacak <tugalsan@gmail.com>
2023-03-21 01:05:40 +0300

    .

--- src/main/java/com/tugalsan/api/charset/client/TGS_CharSetCast.java
+++ src/main/java/com/tugalsan/api/charset/client/TGS_CharSetCast.java
@@ -13,6 +13,9 @@ public class TGS_CharSetCast {
 
     //ISSUE: https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/lang/String.html#toLowerCase()
     public static String toLocaleLowerCase(CharSequence source) {
+        if (source == null) {
+            return null;
+        }
         return TURKISH
                 ? source.toString().toLowerCase(Locale.ROOT)
                 : source.toString().toLowerCase();
@@ -20,16 +23,37 @@ public class TGS_CharSetCast {
 
     //ISSUE: https://docs.oracle.com/en/java/javase/19/docs/api/java.base/java/lang/String.html#toUpperCase()
     public static String toLocaleUpperCase(CharSequence source) {
+        if (source == null) {
+            return null;
+        }
         return TURKISH
                 ? source.toString().toUpperCase(Locale.ROOT)
                 : source.toString().toUpperCase();
     }
 
     public static boolean equalsLocaleIgnoreCase(CharSequence item0, CharSequence item1) {
+        if (item0 == null && item1 == null) {
+            return true;
+        }
+        if (item0 == null && item1 != null) {
+            return false;
+        }
+        if (item0 != null && item1 == null) {
+            return false;
+        }
         return toLocaleUpperCase(item0).equals(toLocaleUpperCase(item1));
     }
 
     public static boolean containsLocaleIgnoreCase(CharSequence fullContent, CharSequence searchTag) {
+        if (fullContent == null && searchTag == null) {
+            return true;
+        }
+        if (fullContent == null && searchTag != null) {
+            return false;
+        }
+        if (fullContent != null && searchTag == null) {
+            return false;
+        }
         return TGS_CharSetCast.toLocaleUpperCase(fullContent).contains(TGS_CharSetCast.toLocaleUpperCase(searchTag));
     }
 
